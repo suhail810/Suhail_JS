a2e3ca73c7fc3b591a94c09326a521a0437cc995
suhail810 <125590332+suhail810@users.noreply.github.com>
2024-02-05 16:25:41 +0000

    basic variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 5747586785
+let accountEmail = "suhail@123"
+var accoutPassword = 890235356
+accountCity = "Pune"
+let accountState;
+console.table([accountId,accountEmail,accoutPassword,accountCity,accountState])
